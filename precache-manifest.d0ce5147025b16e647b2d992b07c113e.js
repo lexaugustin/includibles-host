bc8a61d854484ea1d5e9a2316945f0d944c40994
Alexander Augustin <alexander@alexandersmbp2.lan>
2021-02-26 11:16:05 -0600

    Updates

---
+++ precache-manifest.d0ce5147025b16e647b2d992b07c113e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "254b1cc358efb5104a5bc28767c02b3d",
+    "url": "/includibles-host/index.html"
+  },
+  {
+    "revision": "52ecbcb709b71bf472da",
+    "url": "/includibles-host/static/css/main.9e70bf2a.chunk.css"
+  },
+  {
+    "revision": "6cde053e50452e4da5c4",
+    "url": "/includibles-host/static/js/2.b2986ac6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/includibles-host/static/js/2.b2986ac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ecbcb709b71bf472da",
+    "url": "/includibles-host/static/js/main.443a5a23.chunk.js"
+  },
+  {
+    "revision": "0ba5323977a7a8e90033",
+    "url": "/includibles-host/static/js/runtime-main.6bcc3dfd.js"
+  },
+  {
+    "revision": "60e009d583efef36820d3688375b5fe2",
+    "url": "/includibles-host/static/media/left-arrow.60e009d5.png"
+  },
+  {
+    "revision": "daa3eb895be646d2ccc7fd84a5430c59",
+    "url": "/includibles-host/static/media/right-arrow.daa3eb89.png"
+  }
+]);
